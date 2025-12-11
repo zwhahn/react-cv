0954f04eb1ad10cbb68502ff6dad70f8d2fae620
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-12-11 17:40:55 -0500

    style: display skill list on same line as skill category

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,25 @@
-export function Skills() {
+import { useState } from "react";
+import "../styles/skills.css";
+
+export function Skills({ editable }) {
+  // const [projectTitle, setProjectTitle] = useState("");
+  // const [projectDates, setProjectDates] = useState("");
+  // const [projectDescriptions, setProjectDescriptions] = useState([""]);
+
+  // function handleChange(e, element) {
+  //   if (element === "projectTitle") {
+  //     setProjectTitle(e.target.value);
+  //   } else if (element === "projectDates") {
+  //     setProjectDates(e.target.value);
+  //   }
+  // }
+
   return (
     <>
-      <h3>Languages: </h3>
+      <div className="skill-line">
+        <h3>Languages: </h3>
+        <div>red, blue, green</div>
+      </div>
       <h3>Frameworks & Libraries: </h3>
       <h3>Tools & Platforms: </h3>
     </>
