bd37c4756cd76fdb8eb94e465054031e862830cb
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-12-11 17:13:46 -0500

    feat: make education editable

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,16 +1,64 @@
+import { useState } from "react";
 import "../styles/education.css";
 
-export function Education() {
+export function Education({ editable }) {
+  const [schoolName, setSchoolName] = useState("");
+  const [schoolLocation, setSchoolLocation] = useState("");
+  const [degree, setDegree] = useState("");
+  const [completionDate, setCompletionDate] = useState("");
+
+  function handleChange(e, element) {
+    if (element === "schoolName") {
+      setSchoolName(e.target.value);
+    } else if (element === "schoolLocation") {
+      setSchoolLocation(e.target.value);
+    } else if (element === "degree") {
+      setDegree(e.target.value);
+    } else if (element === "completionDate") {
+      setCompletionDate(e.target.value);
+    }
+  }
   return (
     <>
-      <div className="left-and-right-justify">
-        <h3>School</h3>
-        <div>City</div>
-      </div>
-      <div className="left-and-right-justify">
-        <div>Degree</div>
-        <div>Month Year</div>
-      </div>
+      {editable ? (
+        <>
+          <div className="left-and-right-justify">
+            <input
+              placeholder="School Name"
+              value={schoolName}
+              onChange={(e) => handleChange(e, "schoolName")}
+            ></input>
+            <input
+              placeholder="School's Location (ex. Philadelphia)"
+              value={schoolLocation}
+              onChange={(e) => handleChange(e, "schoolLocation")}
+            ></input>
+          </div>
+          <div className="left-and-right-justify">
+            <input
+              placeholder="Degree"
+              value={degree}
+              onChange={(e) => handleChange(e, "degree")}
+            ></input>
+            <input
+              placeholder="Completion Date (Month YYYY)"
+              value={completionDate}
+              onChange={(e) => handleChange(e, "completionDate")}
+            ></input>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="left-and-right-justify">
+            <h3>{schoolName}</h3>
+            <div>{schoolLocation}</div>
+          </div>
+          <div className="left-and-right-justify">
+            <div>{degree}</div>
+            <div>{completionDate}</div>
+          </div>
+        </>
+      )}
     </>
   );
 }
