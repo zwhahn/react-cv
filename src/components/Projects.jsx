29c99670dc6d75220dad10621f924fde48997cb9
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-12-11 17:33:46 -0500

    feat: make project section editable

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,69 @@
-export function Projects() {
+import { useState } from "react";
+
+export function Projects({ editable }) {
+  const [projectTitle, setProjectTitle] = useState("");
+  const [projectDates, setProjectDates] = useState("");
+  const [projectDescriptions, setProjectDescriptions] = useState([""]);
+
+  function handleChange(e, element) {
+    if (element === "projectTitle") {
+      setProjectTitle(e.target.value);
+    } else if (element === "projectDates") {
+      setProjectDates(e.target.value);
+    }
+  }
+
+  function handleBulletChange(e, index) {
+    const newBullets = [...projectDescriptions];
+    newBullets[index] = e.target.value;
+
+    let trimmed = newBullets.filter((bullet) => bullet.trim() != "");
+    trimmed.push("");
+
+    setProjectDescriptions(trimmed);
+  }
+
   return (
     <>
-      <div className="left-and-right-justify">
-        <h3>Project Name</h3>
-        <div>Month Year - Month Year</div>
-      </div>
-      <ul>
-        <li>Here is a sentence describing a very impressive project I did</li>
-        <li>Here is a sentence describing a very impressive project I did</li>
-        <li>Here is a sentence describing a very impressive project I did</li>
-      </ul>
+      {editable ? (
+        <>
+          <div className="left-and-right-justify">
+            <input
+              placeholder="Project Title"
+              value={projectTitle}
+              onChange={(e) => handleChange(e, "projectTitle")}
+            ></input>
+            <input
+              placeholder="Month YYYY - Month YYYY"
+              value={projectDates}
+              onChange={(e) => handleChange(e, "projectDates")}
+            ></input>
+          </div>
+          <ul>
+            {projectDescriptions.map((bullet, index) => (
+              <li key={index}>
+                <input
+                  placeholder="Project Description"
+                  value={bullet}
+                  onChange={(e) => handleBulletChange(e, index)}
+                ></input>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <>
+          <div className="left-and-right-justify">
+            <h3>{projectTitle}</h3>
+            <div>{projectDates}</div>
+          </div>
+          <ul>
+            {projectDescriptions.map((bullet, index) => (
+              <li key={index}>{projectDescriptions}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
