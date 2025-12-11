a8c5e3c1ae1a84f33abc156d5868f74a0966eab8
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-12-10 21:05:23 -0500

    feat: make entire job title header editable

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,37 +3,80 @@ import "../styles/experience.css";
 
 export function Experience({ editable }) {
   const [companyName, setCompanyName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [jobLocation, setJobLocation] = useState("");
 
   function handleCompanyNameChange(e) {
     setCompanyName(e.target.value);
   }
+  function handleStartDateChange(e) {
+    setStartDate(e.target.value);
+  }
+  function handleJobChange(e) {
+    setJobTitle(e.target.value);
+  }
+  function handleJobLocationChange(e) {
+    setJobLocation(e.target.value);
+  }
 
   return (
     <>
-      <div className="left-and-right-justify">
-        <div className="h3-container">
-          {editable ? (
+      {editable ? (
+        <>
+          <div className="left-and-right-justify">
+            <div className="h3-container">
+              <input
+                placeholder="Company Name"
+                value={companyName}
+                onChange={handleCompanyNameChange}
+              ></input>
+            </div>
             <input
-              placeholder="Company Name"
-              value={companyName}
-              onChange={handleCompanyNameChange}
+              placeholder="Month YYYY - Month YYYY"
+              value={startDate}
+              onChange={handleStartDateChange}
             ></input>
-          ) : (
-            <h3>{companyName}</h3>
-          )}
-        </div>
-        <div>Month Year - Month Year</div>
-      </div>
-      <div className="left-and-right-justify">
-        <div>Job</div>
-        <div>Job Location</div>
-      </div>
-      <ul>
-        <li>Strong sentence describing what was done</li>
-        <li>Strong sentence describing what was done</li>
-        <li>Strong sentence describing what was done</li>
-        <li>Strong sentence describing what was done</li>
-      </ul>
+          </div>
+          <div className="left-and-right-justify">
+            <input
+              placeholder="Job"
+              value={jobTitle}
+              onChange={handleJobChange}
+            ></input>
+            <input
+              placeholder="Job Location"
+              value={jobLocation}
+              onChange={handleJobLocationChange}
+            ></input>
+          </div>
+          <ul>
+            <li>Strong sentence describing what was done</li>
+            <li>Strong sentence describing what was done</li>
+            <li>Strong sentence describing what was done</li>
+            <li>Strong sentence describing what was done</li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <div className="left-and-right-justify">
+            <div className="h3-container">
+              <h3>{companyName}</h3>
+            </div>
+            <div>{startDate}</div>
+          </div>
+          <div className="left-and-right-justify">
+            <div>{jobTitle}</div>
+            <div>{jobLocation}</div>
+          </div>
+          <ul>
+            <li>Strong sentence describing what was done</li>
+            <li>Strong sentence describing what was done</li>
+            <li>Strong sentence describing what was done</li>
+            <li>Strong sentence describing what was done</li>
+          </ul>
+        </>
+      )}
     </>
   );
 }
