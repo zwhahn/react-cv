3749e3aeae4f4461acf85a653b56fae93e7ff753
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-12-12 14:40:41 -0500

    feat: add link editing and automatically hyperlink if url is included

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,15 @@ import { useState } from "react";
 import "../styles/header.css";
 
 export function Header({ editable }) {
-  const links = [
-    "Github",
-    "LinkedIn",
-    "Portfolio",
-    "Email",
-    "Phone Number",
-    "City",
-  ];
-
   const [name, setName] = useState("");
   const [summary, setSummary] = useState("");
+  const [linkArray, setLinkArray] = useState([
+    {
+      type: "",
+      value: "",
+      isLink: false,
+    },
+  ]);
 
   function handleChange(e, element) {
     if (element === "name") {
@@ -22,26 +20,66 @@ export function Header({ editable }) {
     }
   }
 
+  function handleLinkChange(e, index, dataField) {
+    const newLinks = [...linkArray];
+    if (dataField === "type") {
+      newLinks[index].type = e.target.value;
+    } else if (dataField === "link") {
+      newLinks[index].value = e.target.value;
+    }
+
+    if (newLinks[index].value !== "") {
+      newLinks[index].isLink = true;
+    }
+
+    let trimmed = newLinks.filter((link) => link.type.trim() != "");
+    trimmed.push({ type: "", value: "", isLink: false });
+
+    setLinkArray(trimmed);
+  }
+
   return (
     <header>
       {editable ? (
-        <input
-          placeholder="Your Name"
-          value={name}
-          onChange={(e) => handleChange(e, "name")}
-        ></input>
+        <>
+          <input
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => handleChange(e, "name")}
+          ></input>
+          <ul>
+            {linkArray.map((link, index) => (
+              <li key={index}>
+                <input
+                  placeholder="Link"
+                  value={link.type}
+                  onChange={(e) => handleLinkChange(e, index, "type")}
+                ></input>
+                <input
+                  placeholder="Link"
+                  value={link.value}
+                  onChange={(e) => handleLinkChange(e, index, "link")}
+                ></input>
+              </li>
+            ))}
+          </ul>
+        </>
       ) : (
-        <h1>{name}</h1>
+        <>
+          <h1>{name}</h1>
+          <ul>
+            {linkArray.map((link, index) =>
+              link.isLink ? (
+                <li key={index}>
+                  <a href={link.value}>{link.type}</a>
+                </li>
+              ) : (
+                <li key={index}>{link.type}</li>
+              ),
+            )}
+          </ul>
+        </>
       )}
-      <ul>
-        {links.map((link) => {
-          return (
-            <li>
-              <a href="url">{link}</a>
-            </li>
-          );
-        })}
-      </ul>
       {editable ? (
         <input
           placeholder="I am a software engineer with 3 years of experience..."
